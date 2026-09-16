573cb1dd3c0eab44b7b37a5e18cad1cec5fbb3ca
Baruc <barucsinfiltros@gmail.com>
2026-09-16 00:16:06 -0400

    Verificacion de datos en Models/ └── ApplicationUser.cs

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace shipnet.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(100)]
         public string NombreCompleto { get; set; } = string.Empty;
     }
 }
