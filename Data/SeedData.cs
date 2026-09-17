abe5738aa93a7f89c0c08c0aefda9fc11fc868e5
Baruc <barucsinfiltros@gmail.com>
2026-09-16 22:42:11 -0400

    Creacion de "SeedData.cs"

---
+++ Data/SeedData.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace shipnet.Data
+{
+    public static class SeedData
+    {
+        public static async Task InitializeAsync(
+            IServiceProvider serviceProvider)
+        {
+            var roleManager =
+                serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] roles =
+            {
+                "Admin",
+                "Docente",
+                "Estudiante"
+            };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(
+                        new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
